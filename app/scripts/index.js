68e8ef5309caffe06b6b9b07945dc9cee5f80482
hejie03 <hejie03@baidu.com>
2015-09-28 18:24:58 +0800

    prototype was added.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,24 @@
-console.log('ok');
+Vue.component('tree', {
+    // declare the props
+    props: ['apiId'],
+    // the prop can be used inside templates, and will also
+    // be set as `this.msg`
+    template: '<span class="leaf" data-key="{{nodeKey}}" data-value="{{nodeValue}}">' +
+                  '<i class="remove-child">-</i>' +
+                  '<input type="text" class="leaf-key" v-model="nodeKey" placeholder="key" />' +
+                  '<i>---</i>' +
+                  '<input type="text" class="leaf-value" v-model="nodeValue" placeholder="value" />' +
+                  '<i class="add-child">+</i>' +
+                  '<i class="add-sibling">+</i>' +
+              '</span>',
+})
+
+new Vue({
+  el: '#app',
+  data: {
+    message: '',
+    validation: {
+      valid: false
+    }
+  }
+})
